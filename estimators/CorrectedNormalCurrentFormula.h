8a17ed6dac21c0703638639f50b14132d4ebbdf7
Jacques-Olivier Lachaud <jacques-olivier.lachaud@univ-savoie.fr>
2020-02-19 16:30:26 +0100

    Working on CNC

--- estimators/CorrectedNormalCurrentFormula.h
+++ estimators/CorrectedNormalCurrentFormula.h
@@ -63,13 +63,107 @@ namespace DGtal
      corrected normal current formulas of curvatures.
 
      @tparam TDigitalSurfaceContainer any type of digital surface container.
+
+     Formula for interpolated measures:
+
+     MU0=-1/6*((uAz + uBz + uCz)*Bx - (uAz + uBz + uCz)*Cx)*Ay + 1/6*((uAz + uBz + uCz)*Ax - (uAz + uBz + uCz)*Cx)*By - 1/6*((uAz + uBz + uCz)*Ax - (uAz + uBz + uCz)*Bx)*Cy + 1/6*((uAy + uBy + uCy)*Bx - (uAy + uBy + uCy)*Cx - (uAx + uBx + uCx)*By + (uAx + uBx + uCx)*Cy)*Az - 1/6*((uAy + uBy + uCy)*Ax - (uAy + uBy + uCy)*Cx - (uAx + uBx + uCx)*Ay + (uAx + uBx + uCx)*Cy)*Bz + 1/6*((uAy + uBy + uCy)*Ax - (uAy + uBy + uCy)*Bx - (uAx + uBx + uCx)*Ay + (uAx + uBx + uCx)*By)*Cz
+     Let UM=uA+uB+uC
+     MU0=-1/6*(uMz*Bx - uMz*Cx)*Ay + 1/6*(uMz*Ax - uMz*Cx)*By - 1/6*(uMz*Ax - uMz*Bx)*Cy + 1/6*(uMy*Bx - uMy*Cx - uMx*By + uMx*Cy)*Az - 1/6*(uMy*Ax - uMy*Cx - uMx*Ay + uMx*Cy)*Bz + 1/6*(uMy*Ax - uMy*Bx - uMx*Ay + uMx*By)*Cz
+     MU0=1/6*det( UM, B-A, C-A )
+
+
+     MU1=1/6*((uBy - uCy)*uAz - (uAy + 2*uCy)*uBz + (uAy + 2*uBy)*uCz)*Ax + 1/6*((uBy + 2*uCy)*uAz - (uAy - uCy)*uBz - (2*uAy + uBy)*uCz)*Bx - 1/6*((2*uBy + uCy)*uAz - (2*uAy + uCy)*uBz - (uAy - uBy)*uCz)*Cx - 1/6*((uBx - uCx)*uAz - (uAx + 2*uCx)*uBz + (uAx + 2*uBx)*uCz)*Ay - 1/6*((uBx + 2*uCx)*uAz - (uAx - uCx)*uBz - (2*uAx + uBx)*uCz)*By + 1/6*((2*uBx + uCx)*uAz - (2*uAx + uCx)*uBz - (uAx - uBx)*uCz)*Cy + 1/6*((uBx - uCx)*uAy - (uAx + 2*uCx)*uBy + (uAx + 2*uBx)*uCy)*Az + 1/6*((uBx + 2*uCx)*uAy - (uAx - uCx)*uBy - (2*uAx + uBx)*uCy)*Bz - 1/6*((2*uBx + uCx)*uAy - (2*uAx + uCx)*uBy - (uAx - uBx)*uCy)*Cz
+
+     MU2=-1/2*uCx*uBy*uAz + 1/2*uBx*uCy*uAz + 1/2*uCx*uAy*uBz - 1/2*uAx*uCy*uBz - 1/2*uBx*uAy*uCz + 1/2*uAx*uBy*uCz
+     MU2=1/2*( uAx*( uBy*uCz - uCy*uBz ) -uAy*( uBx*uCz - uCx*uBz )+uAz*( uBx*uCy - uCx*uBy ) = 1/2*det( uA, uB, uC )
+
   */
   template < typename TRealPoint, typename TRealVector >
   class CorrectedNormalCurrentFormula
   {
-    typedef TRealPoint  RealPoint;
-    typedef TRealVector RealVector;
+    typedef TRealPoint                     RealPoint;
+    typedef TRealVector                    RealVector;
+    typedef typename RealVector::Component Scalar;
+    typedef std::vector< RealPoint >       RealPoints;
+    typedef std::vector< RealVector >      RealVectors;
+    typedef std::size_t                    Index;
+
+    /// Computes area of triangle abc given a constant
+    /// corrected normal vector \a u.
+    static
+    Scalar areaConstantU
+    ( const RealPoint& a, const RealPoint& b, const RealPoint& c,
+      const RealVector& u )
+    {
+      return ( b - a ).crossProduct( c - a ).dotProduct( u );
+    }
+
+    /// Computes area of triangle abc given an interpolated
+    /// corrected normal vector \a ua, \a \ub, \a uc.
+    static
+    Scalar areaInterpolatedU
+    ( const RealPoint& a, const RealPoint& b, const RealPoint& c,
+      const RealVector& ua, const RealVector& ub, const RealVector& uc )
+    {
+      // mu0 =  1/6 < (u_A + u_B + u_C ) | (AB x AC ) >
+      return 1.0/6.0 * ( b - a ).crossProduct( c - a ).dotProduct( ua + ub + uc );
+    }
     
+    /// Computes area of polygonal face \a pts given a constant
+    /// corrected normal vector \a u.
+    static
+    Scalar areaConstantU( const RealPoints& pts, const RealVector& u )
+    {
+      if ( pts.size() <  3 ) return 0.0;
+      if ( pts.size() == 3 )
+	return areaConstantU( pts[ 0 ], pts[ 1 ], pts[ 2 ], u );
+      const RealPoint b = barycenter( pts );
+      Scalar          a = 0.0;
+      for ( Index i = 0; i < pts.size(); i++ )
+	a += area( b, pts[ i ], pts[ (i+1)%pts.size() ], u );
+      return a;
+    }
+
+    /// Computes area of polygonal face \a pts given an interpolated
+    /// corrected normal vector \a ua, \a \ub, \a uc.
+    static
+    Scalar areaInterpolatedU( const RealPoints& pts, const RealVectors& u )
+    {
+      if ( pts.size() <  3 ) return 0.0;
+      if ( pts.size() == 3 )
+	return areaInterpolatedU( pts[ 0 ], pts[ 1 ], pts[ 2 ],
+				  u[ 0 ], u[ 1 ], u[ 2 ] );
+      const RealPoint   b = barycenter( pts );
+      const RealVector ub = barycenter( pts );
+      Scalar          a = 0.0;
+      for ( Index i = 0; i < pts.size(); i++ )
+	a += areaInterpolatedU( b,  pts[ i ], pts[ (i+1)%pts.size() ],
+				ub,   u[ i ],   u[ (i+1)%pts.size() ] );
+      return a;
+    }
+    
+    /// Given a vector of points, returns its barycenter.
+    /// @param pts any vector of points
+    /// @return the barycenter of these points.
+    static 
+    RealPoint barycenter( const RealPoints& pts )
+    {
+      RealPoint b;
+      for ( auto p : pts ) b += p;
+      b /= pts.size();
+      return b;
+    }
+
+    /// Given a vector of unit vectors, returns their average unit vector.
+    /// @param pts any vector of vectors.
+    /// @return the average unit vector.
+    static 
+    RealVector averageUnitVector( const RealVectors& vecs )
+    {
+      RealVector avg;
+      for ( auto v : vecs ) avg += v;
+      return avg.getNormalized();
+    }
   };
 
 } // namespace DGtal
